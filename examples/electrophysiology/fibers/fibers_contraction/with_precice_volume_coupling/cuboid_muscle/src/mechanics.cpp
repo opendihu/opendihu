c0442332c76280def61a3031c60c223bb58b2cb2
carme-hp <homspons@gmail.com>
2024-03-06 20:01:24 +0100

    Format code with clang

--- examples/electrophysiology/fibers/fibers_contraction/with_precice_volume_coupling/cuboid_muscle/src/mechanics.cpp
+++ examples/electrophysiology/fibers/fibers_contraction/with_precice_volume_coupling/cuboid_muscle/src/mechanics.cpp
@@ -5,18 +5,16 @@
 #include "easylogging++.h"
 #include "opendihu.h"
 
-int main(int argc, char *argv[])
-{
-  // initialize OpenDihu 
+int main(int argc, char *argv[]) {
+  // initialize OpenDihu
   DihuContext settings(argc, argv);
 
   // define MuscleContractionSolver for 3D mechanics
-  Control::PreciceAdapterVolumeCoupling<
-    MuscleContractionSolver< 				
+  Control::PreciceAdapterVolumeCoupling<MuscleContractionSolver<
       Mesh::StructuredDeformableOfDimension<3>,
-      Equation::SolidMechanics::TransverselyIsotropicMooneyRivlinIncompressibleActive3D
-    >
-  > problem(settings);
+      Equation::SolidMechanics::
+          TransverselyIsotropicMooneyRivlinIncompressibleActive3D>>
+      problem(settings);
 
   problem.run();
 
