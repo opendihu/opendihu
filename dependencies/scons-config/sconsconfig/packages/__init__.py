e05dadc28fcc48575741ac567ddada96f28ecf97
carme-hp <homspons@gmail.com>
2024-11-27 10:20:08 +0100

    Add dependencies needed for checkpointing Co-authored-by: Simon Hauser <simon.hauser@helsinki-systems.de>

--- dependencies/scons-config/sconsconfig/packages/__init__.py
+++ dependencies/scons-config/sconsconfig/packages/__init__.py
@@ -2,6 +2,7 @@ from .clock import clock
 from .MPI import MPI
 from .GSL import GSL
 from .HDF5 import HDF5
+from .nlohmannjson import NlohmannJson
 from .FFTW import FFTW
 from .FFTW3 import FFTW3
 from .PETSc import PETSc
@@ -58,3 +59,4 @@ from .xbraid import xbraid
 from .opencor import OpenCOR
 from .precice import precice
 from .std_simd import std_simd
+from .scr import SCR
