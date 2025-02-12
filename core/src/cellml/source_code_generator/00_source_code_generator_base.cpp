b50951f370bbc5026d2bc378d07b2623b87e0de1
carme-hp <71499004+carme-hp@users.noreply.github.com>
2025-02-12 15:38:43 +0100

    Remove parts of logging messages uncompatible with gcc 12 (#86)
    * Remove parts of logging messages uncompatible with gcc 12
    
    * Fix format
    
    * Fix format

--- core/src/cellml/source_code_generator/00_source_code_generator_base.cpp
+++ core/src/cellml/source_code_generator/00_source_code_generator_base.cpp
@@ -81,9 +81,7 @@ void CellmlSourceCodeGeneratorBase::initializeSourceCode(
     // values in the settings
     LOG(WARNING) << "In CellML: There should be " << nParameters_ << " = "
                  << parametersUsedAsAlgebraic_.size() << "+"
-                 << parametersUsedAsConstant_.size()
-                 << " parameters (algebraics: " << parametersUsedAsAlgebraic_
-                 << ", constants: " << parametersUsedAsConstant_ << ") or "
+                 << parametersUsedAsConstant_.size() << " parameters or "
                  << nParameters_ * nInstances_
                  << " parameters in array of struct format, but "
                  << parametersInitialValues.size()
